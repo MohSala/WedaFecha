e0e064ebaf927551c56aee9149d9fb60a3241847
Moh <moh@Mohs-MacBook-Pro.local>
2019-05-03 09:28:30 +0100

    test commit

---
+++ weather/src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import './App.css';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API = "7ffb483eeb385f4f7fc7c51c6dac01f7";
+
+class App extends Component {
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+
+    const API_C = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API}`);
+    const data = await API_C.json();
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <Titles />
+        <Form getWeather={this.getWeather} />
+
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ weather/src/components/Titles.js
@@ -0,0 +1,17 @@
+import React, {
+    Component
+} from 'react';
+
+
+class Titles extends Component {
+    render() {
+        return (
+            <div className="Titles">
+                <h1> Weather Application </h1>
+                <p> Know the weather in a snap </p>
+            </div>
+        );
+    }
+}
+
+export default Titles;
